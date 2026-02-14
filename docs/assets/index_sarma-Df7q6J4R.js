0f56de3e4f9ad3f1c5e6d7dbdb5fe07cb675756f
Doruk Han <eraticoye@gmail.com>
2026-02-14 13:33:47 +0300

    new version

---
+++ docs/assets/index_sarma-Df7q6J4R.js
@@ -0,0 +1,1 @@
+const s="/UFOODS/assets/index_sarma-19MuSEsN.png";export{s as i};
---
+++ docs/assets/products-Dj3_DqxD.js
@@ -0,0 +1,1 @@
+import{c as t,j as e,r}from"./index-O-nQeEHi.js";t.createRoot(document.getElementById("root")).render(e.jsx(r.StrictMode,{}));
